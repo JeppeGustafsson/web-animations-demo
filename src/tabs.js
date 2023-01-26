5bbdf627e34e44a647d82e4275f257873f7d377f
Jesper Gustafsson <jesper.gustafsson@squeed.com>
2023-01-26 17:29:47 +0100

    animate with javascript

--- src/tabs.js
+++ src/tabs.js
@@ -8,56 +8,78 @@ const frontpage = document.getElementById('Home');
 const midpage = document.getElementById('About');
 const lastpage = document.getElementById('Contact');
 const colorBlock = document.getElementById('color-block');
-colorBlock.style.backgroundColor = '#A8E5CF';
 
 menuItems.forEach((item) =>
   item.addEventListener('click', toggleActiveTab)
 );
 
+const showAnimation = [
+  { transform: 'translateX(-120%)' },
+  { transform: 'translateX(0)' },
+];
+
+const hideAnimation = [
+  { transform: 'translateX(0)' },
+  { transform: 'translateX(-120%)' },
+];
+
+const timingNoDelay = {
+  duration: 500,
+  fill: 'forwards',
+  easing: 'ease',
+};
+const timingWithDelay = {
+  duration: 500,
+  delay: 500,
+  fill: 'forwards',
+  easing: 'ease',
+};
+
+const moveTabAnimation = (to) => {
+  if (window.innerWidth < 720) {
+    return [{ transform: `translateX(${to})` }];
+  } else {
+    return [{ transform: `translateY(${to})` }];
+  }
+};
+
 function toggleActiveTab(event) {
-  if (
-    event.target.innerText === frontpage.id &&
-    !frontpage.className.includes('active')
-  ) {
-    frontpage.classList.add('active');
-    midpage.classList.remove('active');
-    lastpage.classList.remove('active');
+  switch (event.target.innerText) {
+    case frontpage.id:
+      frontpage.style.zIndex = 20;
+      midpage.style.zIndex = 1;
+      lastpage.style.zIndex = 0;
 
-    colorBlock.style.backgroundColor = '#A8E5CF';
-    if (window.innerWidth < 720) {
-      colorBlock.style.left = '0';
-    } else {
-      colorBlock.style.top = '0';
-    }
-  }
-  if (
-    event.target.innerText === midpage.id &&
-    !midpage.className.includes('active')
-  ) {
-    frontpage.classList.remove('active');
-    midpage.classList.add('active');
-    lastpage.classList.remove('active');
+      frontpage.animate(showAnimation, timingNoDelay);
+      midpage.animate(hideAnimation, timingWithDelay);
+      lastpage.animate(hideAnimation, timingWithDelay);
 
-    colorBlock.style.backgroundColor = '#FFD3B6';
-    if (window.innerWidth < 720) {
-      colorBlock.style.left = '33%';
-    } else {
-      colorBlock.style.top = '33%';
-    }
-  }
-  if (
-    event.target.innerText === lastpage.id &&
-    !lastpage.className.includes('active')
-  ) {
-    frontpage.classList.remove('active');
-    midpage.classList.remove('active');
-    lastpage.classList.add('active');
+      colorBlock.style.backgroundColor = '#A8E5CF';
+      colorBlock.animate(moveTabAnimation(0), timingNoDelay);
+      break;
+    case midpage.id:
+      midpage.style.zIndex = 20;
+      lastpage.style.zIndex = 0;
+      frontpage.style.zIndex = 1;
 
-    colorBlock.style.backgroundColor = '#FF8B94';
-    if (window.innerWidth < 720) {
-      colorBlock.style.left = '68%';
-    } else {
-      colorBlock.style.top = '68%';
-    }
+      frontpage.animate(hideAnimation, timingWithDelay);
+      midpage.animate(showAnimation, timingNoDelay);
+      lastpage.animate(hideAnimation, timingWithDelay);
+
+      colorBlock.style.backgroundColor = '#FFD3B6';
+      colorBlock.animate(moveTabAnimation('100%'), timingNoDelay);
+      break;
+    case lastpage.id:
+      lastpage.style.zIndex = 20;
+      frontpage.style.zIndex = 1;
+      midpage.style.zIndex = 0;
+
+      frontpage.animate(hideAnimation, timingWithDelay);
+      midpage.animate(hideAnimation, timingWithDelay);
+      lastpage.animate(showAnimation, timingNoDelay);
+
+      colorBlock.style.backgroundColor = '#FF8B94';
+      colorBlock.animate(moveTabAnimation('200%'), timingNoDelay);
+      break;
   }
 }
